56da8cbbed4e37ba15f88303794d5d978dcc3396
bsoist <github@whsjr.soistmann.com>
2023-10-05 10:20:16 -0400

    Wrong language :)

--- snackbar/__init__.py
+++ snackbar/__init__.py
@@ -13,14 +13,12 @@ def compiles():
     """snackbar.c compiles"""
     check50.c.compile("snackbar.c", lcs50=True)
 
-
-
-@check50.check(test_EOF)
+@check50.check(compiles)
 def test_0():
     """input of \"cold brew\", and \"hot dog\" results in $8.00"""
     items = ["cold brew", "hot dog"]
     output = 8.0
-    check50.run("python3 taqueria.py"
+    check50.run("./snackbar"
     ).stdin(items[0], prompt=True
     ).stdin(items[1], prompt=True
     ).stdout(regex(f"{output:.2f}"), f"${output:.2f}", regex=True).kill()
