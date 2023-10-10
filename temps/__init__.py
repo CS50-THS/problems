b961a19d33e14d7fb14c0c78b53476d36aa9cc65
bsoist <github@whsjr.soistmann.com>
2023-10-10 14:42:24 -0400

    just check for exists and compiles

--- temps/__init__.py
+++ temps/__init__.py
@@ -14,13 +14,4 @@ def compiles():
     check50.c.compile("temps.c", lcs50=True)
 
 
-@check50.check(compiles)
-def test0():
-    """input of cold brew and hot dog yields output of $8.00"""
-    check50.run("./temps").stdin(
-        "cold brew\nhot dog\n\n"
-        ).stdout(
-        "Your total cost is: $8.00\n"
-        ).exit(0)
-
 
