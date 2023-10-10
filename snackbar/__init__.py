18ff4c6c3a5d5962ae85be39dc741a672db9156c
Bill Soistmann <bsoist@users.noreply.github.com>
2023-10-10 13:36:45 -0400

    Update __init__.py

--- snackbar/__init__.py
+++ snackbar/__init__.py
@@ -22,6 +22,7 @@ def test_0():
     check50.run("./snackbar"
     ).stdin(items[0], prompt=True
     ).stdin(items[1], prompt=True
+           ).sdin("", prompt=True
     ).stdout(regex(f"{output:.2f}"), f"${output:.2f}", regex=True).kill()
 
 
